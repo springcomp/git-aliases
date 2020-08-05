54377c9bdd615ec4e3582e29a48337961ab4b214
springcomp <springcomp@users.noreply.github.com>
2020-08-05 08:46:41 +0200

    Launch Fork on a specific repository.

--- mk-aliases.ps1
+++ mk-aliases.ps1
@@ -4,4 +4,6 @@ git.exe config --global alias.lol 'log --oneline --decorate --graph'
 git.exe config --global alias.pristine '!git reset --hard && git clean -dffx'
 git.exe config --global alias.stage 'add'
 git.exe config --global alias.unstage 'reset HEAD --'
-git.exe config --global alias.visual '!D:/Users/mlabelle/AppData/Local/Fork/Fork'
+
+$encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes("& D:/Users/`$Env:USERNAME/AppData/Local/Fork/Fork `$PWD"))
+git.exe config --global alias.visual "!powershell.exe -NoProfile -EncodedCommand $encodedCommand"
