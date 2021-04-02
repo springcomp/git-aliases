0b3c6c04e11f4264fc0e03d2097b6f0765b1cc43
springcomp <springcomp@users.noreply.github.com>
2021-04-02 16:04:00 +0200

    New command 'uncommit'.

--- mk-aliases.ps1
+++ mk-aliases.ps1
@@ -1,11 +1,13 @@
-git.exe config --global alias.cm '!git checkout master && git merge --ff-only' 
+git.exe config --global alias.cm '!git fetch --all -p && git checkout master && git merge --ff-only origin/master' 
+git.exe config --global alias.cmn '!git fetch --all -p && git checkout main && git merge --ff-only origin/main' 
+git.exe config --global alias.istage '!git-istage'
 git.exe config --global alias.last 'log -1 HEAD'
 git.exe config --global alias.lol 'log --oneline --decorate --graph'
 git.exe config --global alias.pristine '!git reset --hard && git clean -dffx'
-git.exe config --global alias.rf 'checkout HEAD^1 --'
+git.exe config --global alias.undo 'checkout HEAD^1 --'
 git.exe config --global alias.stage 'add'
-git.exe config --global alias.unstage 'reset HEAD --'
+git.exe config --global alias.unstage 'reset --soft HEAD --'
+git.exe config --global alias.uncommit 'reset --soft HEAD^'
 
-$encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes("& D:/Users/`$Env:USERNAME/AppData/Local/Fork/Fork `$PWD"))
-git.exe config --global alias.visual "!powershell.exe -NoProfile -EncodedCommand $encodedCommand"
+$encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes("& `$Env:LOCALAPPDATA/Fork/Fork `$PWD"))
 git.exe config --global alias.visual "!powershell.exe -NoProfile -EncodedCommand $encodedCommand"
